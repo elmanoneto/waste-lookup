feec1289f664d803b75644c43e1a8d55fa4a3817
Elmano Neto <elmanoneto@gmail.com>
2019-01-18 14:59:39 -0300

    Updates

---
+++ precache-manifest.bf92ba0403c38a2183f780dca8bc7f53.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/waste-lookup/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "9907d18750629a30e0da",
+    "url": "/waste-lookup/static/css/main.f715b755.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/waste-lookup/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a2dd084be76509280272",
+    "url": "/waste-lookup/static/js/1.a2dd084b.chunk.js"
+  },
+  {
+    "revision": "ef1ba729175425a09d57",
+    "url": "/waste-lookup/static/js/runtime~main.ef1ba729.js"
+  },
+  {
+    "revision": "9907d18750629a30e0da",
+    "url": "/waste-lookup/static/js/main.9907d187.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/waste-lookup/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/waste-lookup/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/waste-lookup/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "a2dd084be76509280272",
+    "url": "/waste-lookup/static/css/1.822ef896.chunk.css"
+  },
+  {
+    "revision": "edf6512829ded1807ff9b047feeaf1d2",
+    "url": "/waste-lookup/index.html"
+  }
+];
